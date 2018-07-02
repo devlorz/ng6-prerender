e32045e330ca6dadc4a2439031a8a6122ee87cd1
Nutti Saelor <leelorz6@gmail.com>
2018-07-02 23:38:03 +0700

    add prerender config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition(
+      { appId: 'ng6-prerender' }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
